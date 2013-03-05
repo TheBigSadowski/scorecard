a3140784b038cf0bf4461eb9042e0fb6dc422576
unknown <jsadowski@NEO-PC.sf.stealthlab.net>
2013-03-04 16:56:14 -0800

    abstracting the search method

--- server.js
+++ server.js
@@ -11,27 +11,32 @@ var auth = 'Basic ' + new Buffer(config.user+ ':' + config.password).toString('b
 
 
 function search(jql, callback) {
+    var options = {
+        hostname: 'linkshare.jira.com',
+        path: '/rest/api/latest/search?jql=' + escape(jql),
+        headers: {
+            'Authorization': auth
+        }
+    };
+
+    https.get(options, function (res) {
+        res.setEncoding('utf8');
+        res.data = ''
+        res.on('data', function (chunk) {
+            res.data += chunk;
+        });
+        res.on('end', function () {
+            var data = JSON.parse(res.data);
+            callback(null, data);
+        });
+    });
 }
 
-var options = {
-    hostname: 'linkshare.jira.com',
-    path: '/rest/api/latest/search?jql=Project=QR',
-    headers: {
-        'Authorization': auth
-    }
-};
 
-https.get(options, function (res) {
-    res.setEncoding('utf8');
-    res.data = ''
-    res.on('data', function (chunk) {
-        res.data += chunk;
-    });
-    res.on('end', function () {
-        var data = JSON.parse(res.data);
-        for (var i = 0; i < data.issues.length; i++) {
-            console.log(data.issues[i]);
-            console.log('----------------------------------------------------------------------------');
-        }
-    });
+
+search('Project=QR', function (err, results) {
+    for (var i = 0; i < results.issues.length; i++) {
+        console.log(results.issues[i]);
+        console.log('----------------------------------------------------------------------------');
+    }
 });
